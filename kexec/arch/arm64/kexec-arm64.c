41413f1a777aa60c183d461d2e10b8454bac11cc
Matthias Brugger <mbrugger@suse.com>
2019-05-02 15:33:39 +0200

    fix sprintf

--- kexec/arch/arm64/kexec-arm64.c
+++ kexec/arch/arm64/kexec-arm64.c
@@ -229,7 +229,7 @@ static uint64_t find_purgatory_sink(const char *console)
 		return 0;
 	}
 
-	sprintf(mem, device, "/iomem_base");
+	sprintf(mem, device"%s", "/iomem_base");
 	printf("console memory read from %s\n", mem);
 
 	fd = open(mem, O_RDONLY);
